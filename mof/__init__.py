abe866890572b7eb26c16fcdc3e2197cf5de28e1
Erin Sheldon <erin.sheldon@gmail.com>
2018-08-02 15:05:12 -0400

    remove old versions

--- mof/__init__.py
+++ mof/__init__.py
@@ -1,12 +1,7 @@
-from . import minimof
-from .minimof import MiniMOF
 from . import mof
 from .mof import MOF, MOFStamps
 
 from . import priors
-
-# test of mini version
-from . import minitest
 
 # test of big version
 from . import moftest
