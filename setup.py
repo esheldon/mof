77d1037f2e42dcbc66e45ec29dfac38f8aa5180b
Erin Sheldon <erin.sheldon@gmail.com>
2019-10-07 19:38:57 -0400

    bump version to 0.9.9

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ scripts=[os.path.join('bin',s) for s in scripts]
 setup(
     name="mof",
     packages=['mof', 'mof.tests'],
-    version="0.9.8",
+    version="0.9.9",
     scripts=scripts,
     cmdclass={'build_py': build_py},
 )
